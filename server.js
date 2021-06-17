46248c5a8f53340d82b8ff09c583ddc9445264d2
kurtisjin <cloudnine21@gmail.com>
2021-06-16 19:43:30 -0500

    deploying

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget-tracker", {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
